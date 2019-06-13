5769e6ff5417072af2c509436500d714aa2c16da
Emily Mo <46546633+moemily123819@users.noreply.github.com>
2019-06-12 21:58:48 -0400

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,261 @@
+var msglist = ['Had a great time !', 
+               'Lousy service !', 
+               'The server deserves a good tip !',
+               'Disappointed !',
+               'Be generous !',
+               'Memorable!',
+               'Great food, great service !!!',
+               'The server works hard !',
+               'Never coming back !',
+               'Highly recommended !',
+               'Five stars !',
+               'It was only OK !',
+               'It was so-so'];
+
+msg_change('.sub-msg');
+console.log('msg-chg'); 
+            
+function msg_change(element) {
+
+    var random = Math.floor(Math.random()*msglist.length);
+
+    d3.select(element).append("h6")
+        .style('text-align', 'center')
+        .text(msglist[random]);
+    
+    console.log(msglist[random]); 
+    
+    setInterval(() => {
+        
+        random = Math.floor(Math.random()*msglist.length);
+
+        var textMsg = d3.select(element).select("h6")
+        if (!textMsg.empty()) {
+          textMsg.remove();
+        }
+
+        d3.selectAll(element).append("h6")
+        .style('text-align', "center")
+        .text(msglist[random]);},2000);
+
+     }
+
+    
+
+function clear_end_results() {
+
+    
+   var err_msg = d3.select('.err-msg-1')
+       .classed("hidden", true);
+   err_msg = d3.select('.err-msg-2')
+       .classed("hidden", true);
+   err_msg = d3.select('.err-msg-3')
+       .classed("hidden", true);
+  
+   try {  
+     var end_results = d3.select('#end_results').selectAll('h5');
+     if (!end_results.empty()) {
+          end_results.remove();
+        }
+
+   }
+   catch(err) {
+     console.log('no h5');   
+   }    
+       
+}    
+
+
+
+
+// when submit button is clicked, validation of the screen values will take place; if no error, calc tip / total bill amount
+
+var submit = d3.select("#submit");
+console.log('sumbit', submit);
+
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  console.log('function - submit starts');
+    
+  clear_end_results();
+    
+    
+  var billAmtElement = d3.select("#billAmt");
+
+  // Get the value property of the input element
+  var inputBillAmt = billAmtElement.property("value");
+  console.log('Bill Amt', inputBillAmt);  
+
+   
+  var tipPercElement = d3.select("#tipPerc");
+
+  var inputTipPerc = tipPercElement.property("value");
+  var inputTipPerc = parseInt(inputTipPerc);
+  console.log('Tip Perc', inputTipPerc);  
+    
+
+  var nbrPeopleElement = d3.select("#nbrPeople");
+
+  var inputNbrPeople = nbrPeopleElement.property("value");
+  console.log('Nbr People', inputNbrPeople);  
+    
+    
+  var err = validate_bill(inputBillAmt, inputTipPerc, inputNbrPeople);
+    
+  if (err == 0) {
+      calc_total_bill(inputBillAmt, inputTipPerc,inputNbrPeople);
+  }
+   
+  console.log('location replace');  
+
+  //  location.replace("../");
+  d3.event.preventDefault();
+
+})
+
+
+
+
+
+
+
+// reset - remove answers and border, clear bill amounts and reset tip and number of patrons
+          
+var reset = d3.select("#reset");
+console.log('reset', reset);
+
+reset.on("click", function() {
+  
+   clear_end_results();
+
+   console.log('function - reset starts');
+})
+
+
+
+// bill amount must be positive with at most 2 dec places; tip % should be positive and nbr of patron must be greater than 1
+
+
+function validate_bill(bill_amt, tip_perc, nbr_people) {
+    
+    var err = 0;
+    var temp_bill = +bill_amt;
+    console.log('validate', err, temp_bill);
+
+    if (temp_bill < 0) {
+        console.log('temp_bill <0');
+        err = 1;
+        var err_msg = d3
+         .select(".err-msg-1")
+         .classed("hidden", false);
+     }
+    
+    else {
+        
+        // if bill amount input has more than 2 decimal places, error
+        
+        var bill_2_dec = +(temp_bill).toFixed(2);
+        
+        if (temp_bill != (bill_2_dec)) {
+            console.log('temp_bill and bill_2_dec', temp_bill, bill_2_dec);
+            err = 1;
+            var err_msg = d3
+             .select(".err-msg-1")
+             .classed("hidden", false);
+        }   
+    }
+
+    
+    var temp_tip = +tip_perc;     
+    if (temp_tip < 0) {
+        err = 1;
+        var err_msg = d3
+         .select(".err-msg-2")
+         .classed("hidden", false);
+     }
+ 
+        
+    var temp_people = +nbr_people;     
+    if (temp_people < 0) {
+        err = 1;
+        var err_msg = d3
+         .select(".err-msg-3")
+         .classed("hidden", false);
+     }
+    
+    return err;
+}    
+
+    
+
+
+// when without error, calc tip and total bill amount :
+    
+function calc_total_bill(bill_amt, tip_perc, nbr_people) {
+
+    clear_end_results();
+    
+    var temp_bill = +bill_amt;
+    var temp_bill = +temp_bill.toFixed(2);
+    var temp_tip = +tip_perc;
+    var temp_people = parseInt(nbr_people);
+        
+    console.log('temp_bill', temp_bill);
+    console.log('temp_people', temp_people);
+    console.log('temp_tip', temp_tip);
+        
+     
+    var tip = +((Math.round(temp_bill * temp_tip)/100).toFixed(2));
+    console.log('tip', tip);
+    
+        
+    if (temp_people == 0) {
+            
+        temp_people = 1;
+            
+    }
+        
+    var tip_pp = (tip / temp_people).toFixed(2);
+    console.log('tip_pp', tip_pp, temp_bill, tip);
+    
+    var total_bill_pp = Math.round((temp_bill + tip) * 100 / temp_people);
+    
+    var total_bill_pp = (total_bill_pp / 100).toFixed(2);
+    
+    console.log('tip_pp', tip_pp, tip, total_bill_pp);
+   
+    
+    if (temp_people == 1) {
+        
+        var output_tip = 'Tip is $'+tip_pp;
+        var output_bill = 'Total Bill is $'+total_bill_pp;
+    }
+    else {
+        var output_tip = 'Tip per person is $'+tip_pp;
+        var output_bill = 'Bill amount per person is $'+total_bill_pp;
+    }    
+   
+        
+    var end_results = d3
+        .select("#end_results")
+        .append("h5")
+        .style("margin-left", "15px")
+        .style("margin-right", "15px")
+        .text(output_tip);
+
+    console.log('output', output_tip, end_results);
+        
+    var end_results = d3
+        .select("#end_results")
+        .append("h5")
+        .style("margin-left", "15px")
+        .style("margin-right", "15px")
+        .text(output_bill);
+     console.log('output', output_bill, end_results);
+    
+}    
+    
+
