5258153e4b71b30832ee365f748a3201057015bd
moemily123819 <moemily123819@gmail.com>
2019-06-25 11:46:54 -0400

    precision

--- app.py
+++ app.py
@@ -19,6 +19,26 @@ def home():
     return render_template("index.html")
 
 
+# send the jsonified results
+@app.route("/send", methods=["GET", "POST"])
+def send():
+    if request.method == "POST":
+        billAmt = request.form["billAmt"]
+        nbrPeople = request.form["nbrPeople"]
+        tipPerc = request.form["tipPerc"]
+  
+        inputdata=[{"billAmt" : billAmt,
+              "tipPerc" : tipPerc,
+              "nbrPeople" : nbrPeople
+              }]
+        print('about to redirect')
+        return redirect("/", code=302)
+   
+    return render_template("index.html")
+
+
+#    return jsonify(inputdata)
+
 
 
 if __name__ == "__main__":
